bcc00dfcad5c3affbba2901d7616e80acd400d2b
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-11 21:20:32 +0530

    Add files via upload

---
+++ DAY-11/Arrays.js
@@ -0,0 +1,17 @@
+const nums = [8,3,5,6,7,1];
+console.log(nums.sort());
+console.log(nums.slice(2,5));
+// console.log(words.indexOf(12));
+// console.log(words.includes(false));
+// console.log(words.push(5));
+// words.unshift(13);
+// console.log(words);
+// console.log(words.pop());
+// console.log(words.shift());
+
+
+// const newWords=words;
+// newWords[1]=13;
+// console.log(words);
+// console.log(words.length);
+ 
---
+++ DAY-11/Loops.js
@@ -0,0 +1,22 @@
+ ///Loops in JS
+// for(let i=0;i<6;i++){
+//     console.log("Anuj",i);
+// }
+// let i=0;
+// while(i<6){
+//     console.log("Anuj",i);
+//     i++;
+// } 
+// let i=0; 
+// do{
+//     console.log("Anuj",i);
+//     i++;
+
+// }while(false);
+let i=1;
+while(i<=5){
+    console.log(i);
+    
+    i++;   
+    if(i==5)break;
+}
